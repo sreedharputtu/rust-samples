6fdf263d31fc0638a2b52c0159efe0a9bd005fd7
sreedharputtu <sreedharputtu@gmail.com>
2022-01-03 16:09:09 +0530

    added comments

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,58 @@
 #![allow(unused_imports)]
 
 use std::mem;
-use std::mem::transmute;
 
 fn main() {
+    /**
+    Signed and unsigned
+    1 . u - un signed -> for example u8 size is 0...256
+    2 . i - signed -> for example 18 size is -128...127
+    unsigned types
+    u8 , u16 , u32 etc
+    signed types
+    i8 , i16 , i32 etc
+     **/
 
-    // u - un signed -> 0...256
-    // i - signed -> -128...127
-    //u8 , u16 , u32 ... i8,i16,i32 etc
-   let s :u8=255; // this creates an immutable variable
-    println!("{}",s);
-    //if you re assign the value to s , compiler will fail
-    //s = 122;s
+    let s: u8 = 255; // this creates an immutable variable
+    println!("{}", s);
+    //if you re assign the value to s ,compilation will fail
+    //s = 122;
 
-    let p :i8 = 127;
-    println!("{}",p);
+    let p: i8 = 127;
+    println!("{}", p);
 
+    /**
+    mutable variable
+    **/
+
+    let mut q: u8 = 256;
+    q = 78;
+    println!("{}", q);
+
+    /**
+    if type is not mentioned explicitly , then rust compiler defaults it automatically
+    in below case c is i32 type
+    **/
     let c = -1;
-    println!("{},{}",c,mem::size_of_val(&c));
-    //if you dont mention type , IDE/compiler defaulting value to inteeger 32 bit signed
+    println!("{},{}", c, mem::size_of_val(&c));
 
-    let d:usize = 1;
+    /**
+    usize and isize are data types which  depends on OS
+    (32 bit or 64 bit based on OS)
+    **/
+    let d: usize = 1;
     let size_of_d = mem::size_of_val(&d);
-    println!("{} bit",size_of_d*8);
+    println!("{} bit", size_of_d * 8);
 
+    /**
+    floating points , f64 is default type for floating point variable
+    f32 , f64 etc
+    **/
     let g = 6.7;
-    println!("{}",mem::size_of_val(&g));
-
-   let x = (1 |  0);
- println!("{},{}",x , mem::size_of_val(&x));
-
- let mut xp:u8 = 22;
-  xp +=2;
- println!("{}",xp);
-
+    println!("{}", mem::size_of_val(&g));
+    /**
+    bool
+    **/
+    let x = true;
+    println!("{},{}", x, mem::size_of_val(&x));
 }
